dd3f83520d14d1548118cea864ce615f4f2bdee5
IsmaelFV <ismaelfloresvargas22@gmail.com>
2025-03-06 10:54:05 +0100

    Arreglo manejo de errores de variable telefono Contacto.java

--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 /**
 * Clase principal que ejecuta el programa del Gestor de Contactos. 
 * Proporciona un menú interactivo para agregar, editar, buscar y listar contactos.
